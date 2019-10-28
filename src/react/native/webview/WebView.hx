16bf9567ab79392ccf039f91e7924b619e6f6a31
Kevin Leung <kevinresol@gmail.com>
2019-10-29 00:22:04 +0800

    Init

---
+++ src/react/native/webview/WebView.hx
@@ -0,0 +1,61 @@
+package react.native.webview;
+
+import react.native.component.props.*;
+import react.ReactComponent;
+import haxe.extern.EitherType;
+import haxe.Constraints;
+
+@:jsRequire('react-native-webview', 'default')
+extern class WebView extends ReactComponentOfProps<WebViewProps> {}
+
+@:enum
+abstract MixedContentModeTypes(String)  {
+	var Never = "never";
+	var Always = "always";
+	var Compatibility = "compatibility";
+}
+
+@:enum
+abstract WebViewDataDetectorType(String) {
+	var PhoneNumber = 'phoneNumber';
+	var Link = 'link';
+	var Adress = 'address';
+	var CalendarEvent = 'calendarEvent';
+	var None = 'none';
+	var All = 'all';
+}
+
+typedef WebViewProps = {
+	> ViewProps,
+	?automaticallyAdjustContentInsets:Bool,
+	?contentInset: {top:Float, left:Float, bottom:Float, right:Float},
+	// ?html:String, // deprecated
+	?injectJavaScript:Function,
+	?injectedJavaScript:String,
+	?mediaPlaybackRequiresUserAction:Bool,
+	?onError:Function,
+	?onLoad:Function,
+	?onLoadEnd:Function,
+	?onLoadStart:Function,
+	?onMessage:Function,
+	?onNavigationStateChange:Function,
+	?renderError:Function,
+	?renderLoading:Function,
+	?scalesPageToFit:Bool,
+	?source: EitherType<{?uri:String, ?method:String, ?headers:Dynamic, ?body:String}, EitherType<{?html:String, ?baseUrl:String}, Float>>,
+	?startInLoadingState:Bool,
+	// ?url:String, // deprecated
+	// android
+	?domStorageEnabled:Bool,
+	?javaScriptEnabled:Bool,
+	?mixedContentMode: MixedContentModeTypes,
+	?userAgent:String,
+	// ios
+	?allowsInlineMediaPlayback:Bool,
+	?bounces:Bool,
+	?dataDetectorTypes:EitherType<WebViewDataDetectorType, Array<WebViewDataDetectorType>>,
+	?decelerationRate:Dynamic,
+	?onShouldStartLoadWithRequest:Function,
+	?scrollEnabled:Bool,
+	?useWebKit:Bool,
+}
